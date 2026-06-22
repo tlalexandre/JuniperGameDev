078b11567aefb6b1ef5d8eac629da3f17476a1dc
tlalexandre <120526785+tlalexandre@users.noreply.github.com>
2026-06-22 18:15:21 +0100

    Added explosion sprite to the bullet_fire

--- Bullet_Fire/bullet_fire.gd
+++ Bullet_Fire/bullet_fire.gd
@@ -1,7 +1,11 @@
 extends Bullet
+
+const EXPLOSION = preload("uid://djok2etes861s")
+
 @export var explosion_radius: float = 80.0
 @export var explosion_damage: int = 2
 var _show_debug_circle := false
+
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
 		_explode()
@@ -25,7 +29,16 @@ func _explode() -> void:
 			hit.take_damage(explosion_damage)
 	_show_debug_circle = true
 	queue_redraw()  # triggers _draw() this frame
-
+	_play_explosion_vfx()
+	
 func _draw() -> void:
 	if _show_debug_circle:
 		draw_circle(Vector2.ZERO, explosion_radius, Color(1, 0.3, 0, 0.4))
+		
+func _play_explosion_vfx() -> void:
+	var vfx = EXPLOSION.instantiate()
+	GlobalData.world.add_child(vfx)
+	vfx.global_position = global_position
+	var sprite_half_width = 16.0  # 32px frame / 2
+	var target_scale = explosion_radius / sprite_half_width
+	vfx.scale = Vector2(target_scale, target_scale)
