54341d12acdaed558a80969975f243fe88b4c86f
NuruNuru <gamedev.slimypixels@gmail.com>
2026-06-30 20:25:03 +0100

    Version 0.0.2
    Bux fix
    
    Fixed bug where ghost ignores the player even if in range
    
    Fixed bug where bullets cannot go through portals
    
    Fixed bug where fireballs go trough walls

--- Portal/portal.gd
+++ Portal/portal.gd
@@ -4,4 +4,8 @@ func _on_body_entered(body: Node2D) -> void:
 	
 	if body.is_in_group("player"):
 		body.get_node("Gun").shoot()#Thanks Jaime
-	GlobalData.world.advance_level()
+		body.get_node("Gun").BulletTypes = [GlobalData.BULLET, GlobalData.BULLET, GlobalData.BULLET,GlobalData.BULLET, GlobalData.BULLET, GlobalData.BULLET]
+		body.get_node("Gun").reload()
+		GlobalData.barrel_hud.update_icons_from_chamber(body.get_node("Gun").BulletTypes)
+		GlobalData.barrel_hud.reset()
+		GlobalData.world.advance_level()
